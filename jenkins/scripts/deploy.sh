d97b9a17f68055172677636c21febbb332e0ca3f
BestCrochet <alicia159@live.com.sg>
2022-11-10 20:03:50 +0800

    fok

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v "/c/SIT/Year 3/3203/Lab7/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html" php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v "/php-selenium-test/src:/var/www/html" php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://172.17.0.2 to see your PHP application in action.'
 
